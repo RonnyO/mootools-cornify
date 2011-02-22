98a580373f132411531d31fb1e0a210e5e3c4ee6
Ronny Orbach <ronny@metacafe.com>
2011-02-22 10:37:05 +0200

    minify + demo with inline minified script

---
+++ mootools-cornify-min.js
@@ -0,0 +1,2 @@
+(function(){var kkeys=[],knm="38,38,40,40,37,39,37,39,66,65",doc=document;function keydown(e){kkeys.push(e.code);if(kkeys.join(',').indexOf(knm)>-1){window.removeEvent('keydown',keydown);new Element('script',{src:"http://www.cornify.com/js/cornify.js",events:{'load':function(){if(window.cornami)clearTimeout(window.cornami.clear);window.addEvent('keydown',(!cornify_add()&&cornify_add));}}}).inject(document.head);}}
+window.addEvent('keydown',keydown);})();
