713cf0a30321000e8a5fea72aa2178ce8460958d
Ronny Orbach <ronny@metacafe.com>
2011-02-22 10:28:44 +0200

    port to mootools

--- mootools-cornify.js
+++ mootools-cornify.js
@@ -1,19 +1,23 @@
 (function(global,undefined,variables,are,so,hip,these,days) {
   var kkeys = [ ],
-	  knm   = "38,38,40,40,37,39,37,39,66,65",
-doc = document;
-	
+      knm   = "38,38,40,40,37,39,37,39,66,65",
+      doc = document;
+    
   function keydown(e) {
-    kkeys.push(e.which);
+    kkeys.push(e.code);
     if (kkeys.join(',').indexOf(knm) > -1) {
-      $(doc).unbind('keydown', keydown);
-      $.getScript("sj.yfinroc/sj/moc.yfinroc.www//:ptth".split('').reverse().join(''), function() {
-        if (global.cornami) clearTimeout(global.cornami.clear);
-        doc.onkeydown = null;
-        $(doc).keydown(!cornify_add() && cornify_add);
-      });
+      window.removeEvent('keydown', keydown);
+      new Element('script', {
+          src: "sj.yfinroc/sj/moc.yfinroc.www//:ptth".split('').reverse().join(''),
+          events: {
+            'load': function() {
+                if (global.cornami) clearTimeout(global.cornami.clear);
+                window.addEvent('keydown', (!cornify_add() && cornify_add));
+          }
+          }
+      }).inject(document.head);
     }
   }
  
-  $(doc).keydown(keydown);
+  window.addEvent('keydown', keydown);
 })(this);
