cd293310fcc94b66c8eb18a6dd44abce9c7cecb1
Ronny Orbach <ronny@metacafe.com>
2011-02-22 10:45:36 +0200

    shorten minified version

--- mootools-cornify-min.js
+++ mootools-cornify-min.js
@@ -1,2 +1,2 @@
-(function(){var kkeys=[],knm="38,38,40,40,37,39,37,39,66,65",doc=document;function keydown(e){kkeys.push(e.code);if(kkeys.join(',').indexOf(knm)>-1){window.removeEvent('keydown',keydown);new Element('script',{src:"http://www.cornify.com/js/cornify.js",events:{'load':function(){if(window.cornami)clearTimeout(window.cornami.clear);window.addEvent('keydown',(!cornify_add()&&cornify_add));}}}).inject(document.head);}}
-window.addEvent('keydown',keydown);})();
+(function(g){var kkeys=[],knm="38,38,40,40,37,39,37,39,66,65",d=document;function keydown(e){kkeys.push(e.code);if(kkeys.join(',').indexOf(knm)>-1){g.removeEvent('keydown',keydown);new Element('script',{src:"http://www.cornify.com/js/cornify.js",events:{'load':function(){if(g.cornami)clearTimeout(g.cornami.clear);g.addEvent('keydown',(!cornify_add()&&cornify_add));}}}).inject(d.head);}}
+g.addEvent('keydown',keydown);})(this);
